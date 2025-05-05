50bfc46b8a14c760c82c396c1b0fd3a0059fae4c
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-05-05 12:33:13 +0200

    feat(core): implement basic /api/ test route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 
 // === SETUP === //
+
+// NOTE: dotenv only work when the server.js file is launch from the root folder (e.g: "node src/server.js")
 dotenv.config();
 const app = express();
 
@@ -9,7 +11,12 @@ const app = express();
 
 // === ROUTER === //
 
+app.get("/api", (req, res) => {
+  res.status(200).send("Basic api request work perfectly !");
+});
+
 // === SERVER === //
+
 app.listen(process.env.HABIT_TRACKER_PORT, () =>
   console.log(
     `The application's server is running on: http://127.0.0.1:${process.env.HABIT_TRACKER_PORT}`
